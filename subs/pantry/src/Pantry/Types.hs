2a2e8bec952676cecb1c284a7fd5de8671a35af1
Michael Snoyman <michael@snoyman.com>
2019-04-04 10:19:08 +0300

    Fix a warning

--- subs/pantry/src/Pantry/Types.hs
+++ subs/pantry/src/Pantry/Types.hs
@@ -125,7 +125,6 @@ import Data.Aeson.Extended
 import Data.ByteString.Builder (toLazyByteString, byteString, wordDec)
 import Database.Persist
 import Database.Persist.Sql
-import Database.Sqlite (SqliteException)
 import Pantry.SHA256 (SHA256)
 import qualified Pantry.SHA256 as SHA256
 import qualified Distribution.Compat.ReadP as Parse
