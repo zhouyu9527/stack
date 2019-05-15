370cd701c262007f807e1fb9971c3f3b695a2881
Kirill Zaborsky <qrilka@gmail.com>
2019-05-15 15:48:32 +0300

    Improvements for #4768 test case

--- test/integration/tests/4768-reconfigure-bug/Main.hs
+++ test/integration/tests/4768-reconfigure-bug/Main.hs
@@ -5,7 +5,8 @@ main :: IO ()
 main = do
   stack ["clean", "--full"]
   copyFile "a/to-copy/A1.hs" "a/src/A.hs"
-  stack ["build"]
+  stack ["build", "a"]
+  stack ["build", "c"]
 
   copyFile "a/to-copy/A2.hs" "a/src/A.hs"
   stack ["build", "a"]
